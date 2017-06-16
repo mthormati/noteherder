428ad6bef4e8ee71856a98908af67716ffa2a1a5
Matin Hormati <mthormati@gmail.com>
2017-06-15 22:41:08 -0400

    Change noteherder to a dark theme because light themes suck

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 import quill from './quill.svg'
-import newHover from './new-hover.png'
-import newIcon from './new.png'
+import newHover from './new-hover-dark.png'
+import newIcon from './new-dark.png'
 import './Sidebar.css'
 
-const Sidebar = () => {
+
+class Sidebar extends Component {
+
+  clickHandler = () => {
+    
+  }
+
+  render() {
     return (
       <nav className="Sidebar">
         <div className="logo">
           <img src={quill} alt="Noteherder" />
         </div>
-        <button className="new-note">
+        <button className="new-note" onClick={this.clickHandler}>
           <img src={newHover} alt="New note" />
           <img className="outline" src={newIcon} alt="New note" />
         </button>
       </nav>
     )
+  }
 }
 
-export default Sidebar
+export default Sidebar;
