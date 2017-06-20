14f3e947d230f213f8208ca4d2ecbdf32f50eadd
Matin Hormati <mthormati@gmail.com>
2017-06-20 11:52:27 -0400

    Add styles to sign in page

--- src/SignIn.js
+++ src/SignIn.js
@@ -9,14 +9,15 @@ const SignIn = () => {
     if (e.currentTarget.id === "github") {
       auth.signInWithPopup(githubProvider)
     } else {
-      auth.signInWithPopup(googleProvider)
+      auth.signInWithRedirect(googleProvider)
     }
   }
 
   return (
     <div>
       <Particles params={ particles } style={{position: "absolute"}}/>
-      <div id="button-container">
+
+      <div id="button-container" className="animated fadeInDown">
 
         <div id="title">NoteHerder</div>
 
